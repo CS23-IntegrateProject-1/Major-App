aaefcf0ef33515a6e3356789d679e6dd030353b9
kayjirat <kayjirat12@gmail.com>
2023-11-11 15:17:42 +0700

    [ADD] create movie page
    -create movie page
    -just NOW SHOWING button

--- frontend-major/src/pages/Major/MajorRoutes.tsx
+++ frontend-major/src/pages/Major/MajorRoutes.tsx
@@ -1,10 +1,15 @@
 import ExamplePage from "./pages/ExamplePage";
+import MoviePage from "./pages/Movies";
 
 const ExampleRoutes = () => {
 	return [
 		{
 			path: "/Example",
 			element: <ExamplePage />
+		},
+		{
+			path: "/Movie",
+			element: <MoviePage/>
 		}
 	];
 };
---
+++ frontend-major/src/pages/Major/pages/Movies.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@chakra-ui/react";
+import { TextStyle } from "../../../theme/TextStyle";
+import { useState } from "react";
+
+const MoviePage = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const linkStyles = {
+    ...TextStyle.h3,
+    color: isHovered ? "#d2aa5a" : "white", // Color changes on hover
+    textDecoration: "none",
+    transition: "color 0.3s ease",
+    padding: "2vh",
+  }
+  const handleMouseOver = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovered(false);
+  };
+
+
+  return (
+    <Box>
+      <a
+        href="/Movie"
+        style={linkStyles}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        NOW SHOWING
+      </a>
+    </Box>
+    
+  );
+};
+
+export default MoviePage;
