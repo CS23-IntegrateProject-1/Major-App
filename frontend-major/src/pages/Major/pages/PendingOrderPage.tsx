7897b5382c56adb2423336085df8050304fafbf2
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-12-18 22:08:44 +0700

    fix unused index

--- frontend-major/src/pages/Major/pages/PendingOrderPage.tsx
+++ frontend-major/src/pages/Major/pages/PendingOrderPage.tsx
@@ -4,37 +4,37 @@ import { useLocation } from "react-router-dom";
 import { TypeOfSeat2 } from "../../../components/MovieSeat/TypeOfSeat2";
 
 function PendingOrderPage() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const seatTypes = queryParams.get("seatTypes")?.split(",") || [];
-  const totalPrice = queryParams.get("totalPrice")?.split(",") || [];
+	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search);
+	const seatTypes = queryParams.get("seatTypes")?.split(",") || [];
+	const totalPrice = queryParams.get("totalPrice")?.split(",") || [];
 
-  const uniqueSeatTypesMap = new Map<string, boolean>(); // Using Map to store unique seat types
+	const uniqueSeatTypesMap = new Map<string, boolean>(); // Using Map to store unique seat types
 
-  // Loop through seatTypes to extract unique seat types
-  seatTypes.forEach((type, index) => {
-    const [typeName] = type.split(":");
+	// Loop through seatTypes to extract unique seat types
+	seatTypes.forEach((type) => {
+		const [typeName] = type.split(":");
 
-    // Check if the seat type name already exists in the Map
-    if (!uniqueSeatTypesMap.has(typeName)) {
-      uniqueSeatTypesMap.set(typeName, true); // Store the unique seat type with its price
-    }
-  });
+		// Check if the seat type name already exists in the Map
+		if (!uniqueSeatTypesMap.has(typeName)) {
+			uniqueSeatTypesMap.set(typeName, true); // Store the unique seat type with its price
+		}
+	});
 
-  const uniqueSeatTypes = Array.from(uniqueSeatTypesMap.entries()); // Convert Map back to array of entries
+	const uniqueSeatTypes = Array.from(uniqueSeatTypesMap.entries()); // Convert Map back to array of entries
 
-  return (
-    <Box>
-      <Text color="gold" {...TextStyle.h1} mb={4}>
-        Purchase Order
-      </Text>
+	return (
+		<Box>
+			<Text color="gold" {...TextStyle.h1} mb={4}>
+				Purchase Order
+			</Text>
 
-      {uniqueSeatTypes.map(([typeName], index) => (
-        <TypeOfSeat2 key={index} type={{ typeName }} />
-      ))}
-      <Box>Total Price: {totalPrice.join(", ")}</Box>
-    </Box>
-  );
+			{uniqueSeatTypes.map(([typeName], index) => (
+				<TypeOfSeat2 key={index} type={{ typeName }} />
+			))}
+			<Box>Total Price: {totalPrice.join(", ")}</Box>
+		</Box>
+	);
 }
 
 export default PendingOrderPage;
