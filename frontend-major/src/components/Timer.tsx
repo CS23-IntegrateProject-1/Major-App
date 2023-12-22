046a79df468bb65a8d59ef9113c05f9865d8c6a0
kayjirat <kayjirat12@gmail.com>
2023-12-22 23:37:17 +0700

    [EDIT] timer

--- frontend-major/src/components/Timer.tsx
+++ frontend-major/src/components/Timer.tsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-const Timer: React.FC<{ expiryTimestamp: number }> = ({ expiryTimestamp }) => {
-  const calculateTimeLeft = () => {
-    const difference = expiryTimestamp - Date.now();
-    let timeLeft: { minutes: number; seconds: number } = { minutes: 0, seconds: 0 };
+// const Timer: React.FC<{ expiryTimestamp: number }> = ({ expiryTimestamp }) => {
+//   const calculateTimeLeft = () => {
+//     const difference = expiryTimestamp - Date.now();
+//     let timeLeft: { minutes: number; seconds: number } = { minutes: 0, seconds: 0 };
 
-    if (difference > 0) {
-      timeLeft = {
-        minutes: Math.floor((difference / 1000) / 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
+//     if (difference > 0) {
+//       timeLeft = {
+//         minutes: Math.floor((difference / 1000) / 60),
+//         seconds: Math.floor((difference / 1000) % 60),
+//       };
+//     }
 
-    return timeLeft;
-  };
+//     return timeLeft;
+//   };
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+//   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setTimeLeft(calculateTimeLeft());
+//     }, 1000);
 
-    return () => clearTimeout(timer);
-  }, [timeLeft, expiryTimestamp]);
+//     return () => clearTimeout(timer);
+//   }, [timeLeft, expiryTimestamp]);
 
-  return (
-    <div>
-      Time Left: {timeLeft.minutes}:{timeLeft.seconds < 10 ? `0${timeLeft.seconds}` : timeLeft.seconds}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       Time Left: {timeLeft.minutes}:{timeLeft.seconds < 10 ? `0${timeLeft.seconds}` : timeLeft.seconds}
+//     </div>
+//   );
+// };
 
-export default Timer;
+// export default Timer;
