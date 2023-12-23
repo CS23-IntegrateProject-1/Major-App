9c609db7cbdc342bbb796919517f1c58e667022e
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-12-24 03:53:34 +0700

    Add delete reservation

--- backend-major/src/routes/reservation.routes.ts
+++ backend-major/src/routes/reservation.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { checkAvailability, createReservation, getReserveSeatByShowId } from "../controllers/reservationController";
+import { checkAvailability, createReservation, deleteReservation, getReserveSeatByShowId } from "../controllers/reservationController";
 
 const reservation = Router();
 
 reservation.post("/create", createReservation);
 reservation.post("/check", checkAvailability);
 reservation.post("/getReserveSeatByShowId", getReserveSeatByShowId);
+reservation.post("/delete", deleteReservation)
 
 export default reservation;
